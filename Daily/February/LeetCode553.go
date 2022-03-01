8296e853a356d3f3656eeb07bd629f107bbd7e46
Shuimo03 <wu1998102@gmail.com>
2022-03-01 16:05:02 +0800

    2月打卡成就

---
+++ Daily/February/LeetCode553.go
@@ -0,0 +1,17 @@
+package february
+
+import "strconv"
+
+func optimalDivision(nums []int) string {
+	if len(nums) == 0 {
+		return ""
+	}
+	if len(nums) == 1 {
+
+	}
+}
+
+func intToString(a int) string {
+	res := strconv.Itoa(a)
+	return res
+}
