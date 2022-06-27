7dc5b20266dc927e8b26b74a90ffb5138cceaf73
Shuimo03 <wu1998102@gmail.com>
2022-06-27 21:09:58 +0800

    字符串

---
+++ String/LeetCode14.go
@@ -0,0 +1,31 @@
+package string
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0] // 假设第一个值就是公共前缀
+	for i := 1; i < len(strs); i++ {
+		prefix = lcp(prefix, strs[i])
+		if len(prefix) == 0 {
+			break
+		}
+	}
+	return prefix
+}
+
+func lcp(str1, str2 string) string {
+	length := min(len(str1), len(str2))
+	index := 0
+	for index < length && str1[index] == str2[index] {
+		index++
+	}
+	return str1[:index]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
