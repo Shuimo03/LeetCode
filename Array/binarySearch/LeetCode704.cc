7c93750ab51afe6b5c40af3da2d82454b3d38a8e
Shuimo03 <wu1998102@gmail.com>
2022-07-04 08:31:28 +0800

    二分查找

---
+++ Array/binarySearch/LeetCode704.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int left = 0, right = nums.size()-1;
+       while(left <= right){
+            int mid = (left+right) >> 1;
+            if(nums[mid] == target){
+                right =  mid;
+            }else{
+                left = mid +1;
+            }
+       }
+       return l;
+    }
+};
