54aa8b674d2e94106ccf4e13684740049867b7f7
Shuimo03 <wu1998102@gmail.com>
2022-08-08 19:35:40 +0800

    数组

--- Array/LeetCode88.cc
+++ Array/LeetCode88.cc
@@ -5,12 +5,16 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-      int pos = m--+n-- -1;
-      while (m >= 0 && n >= 0){
-          nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--]:nums2[n--];
-      }
-      while (n >= 0){
-          nums1[pos--] = nums2[n--];
-      }
+        int i = m-1,j = n-1;
+        for(int k = m+n-1; k >= 0; k--){
+            //处理边界
+            if(j < 0 || (i >= 0 && nums1[i] >= nums2[j])){
+                nums1[k] = nums1[i];
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+        }
     }
 };
